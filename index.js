8844e7b84e15a27263dfa9e92ed36ded729c3a26
Cameron Bytheway <cameron@nujii.com>
2013-07-13 15:03:25 -0600

    Clean up unused files

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-
 /**
  * Module dependencies.
  */
+
 var join = require("path").join;
 
 /**
  * Expose the layout
  */
-module.exports.layout = join(__dirname, "views", "layout.jade");
+
+module.exports.layout = join(__dirname, "layout.jade");
 
 /**
  * Favicon
  */
+
 module.exports.favicon = join(__dirname, "assets", "images", "favicon.png");
